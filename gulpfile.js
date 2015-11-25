06aa0ac7b539cb66aaecfde2518124d0c359c0ac
Drm1804 <drm-a@yandex.ru>
2015-11-25 09:38:51 +0500

    Добавил отслеживание изменения JS, поправил README.md

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var mainBowerFiles = require('main-bower-files');
 var filter = require('gulp-filter');
-var app = './dist/';
+var dist = './dist/';
 
 gulp.task('vendors', function () {
     var mainFiles = mainBowerFiles();
@@ -12,47 +12,62 @@ gulp.task('vendors', function () {
     return gulp.src(mainBowerFiles())
         .pipe(filter('**/*.js'))
         .pipe(concat('vendor.js'))
-        .pipe(gulp.dest(app + 'scripts'));
+        .pipe(gulp.dest(dist + 'scripts'));
 });
 
 gulp.task('scripts', function () {
-    gulp.src('app/script/**/*.js')
+    gulp.src('app/script/**/*')
         .pipe(concat('script.js'))
-        .pipe(gulp.dest(app + 'scripts'))
+        .pipe(gulp.dest(dist + 'scripts'))
         .pipe(connect.reload());
 });
 
 gulp.task('css', function () {
     gulp.src('app/css/**/*.css')
         .pipe(concat('style.css'))
-        .pipe(gulp.dest(app + 'css'))
+        .pipe(gulp.dest(dist + 'css'))
         .pipe(connect.reload());
 });
 
 gulp.task('html', function(){
-   gulp.src('app/**/*.html')
-       .pipe(gulp.dest(app))
-       .pipe(connect.reload());
+    gulp.src('app/**/*.html')
+        .pipe(gulp.dest(dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('fonts', function(){
+    gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest(dist + 'fonts'))
+        .pipe(connect.reload());
+});
+
+gulp.task('img', function(){
+    gulp.src('app/img/**/*')
+        .pipe(gulp.dest(dist + 'img'))
+        .pipe(connect.reload());
 });
 
 gulp.task('connect', function () {
     connect.server({
         livereload: true,
-        root: app,
+        root: dist,
         port: 8000
     });
 });
 
 gulp.task('watch',function(){
     gulp.watch('app/**/*.css',['css']);
-    gulp.watch('js/*.js',['scripts']);
-    gulp.watch('**/*.html',['html']);
+    gulp.watch('app/**/*.js',['scripts']);
+    gulp.watch('app/**/*.html',['html']);
 });
 
 gulp.task('default', [
     'vendors',
     'watch',
     'css',
+    'scripts',
+    'fonts',
+    'img',
     'html',
     'connect'
 ]);
